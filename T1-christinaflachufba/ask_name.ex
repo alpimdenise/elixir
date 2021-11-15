57358178a3f1eb2396e88482d8e30f4dc81963c6
DENISE A S SILVA <80422358+alpimdenise@users.noreply.github.com>
2021-11-15 16:41:25 -0300

    ask_name.ex
    adição T1 E1 ask_name.ex

--- T1-christinaflachufba/ask_name.ex
+++ T1-christinaflachufba/ask_name.ex
@@ -1,1 +1,28 @@
 # Ask Name
+#Programa como perguntar um nome
+T1E1 primeira forma
+IO.puts("Qual é o seu nome?")
+       valorDigitado = IO.gets("");
+IO.puts("Ola #{String.trim(valorDigitado)}, seja bem-vindo!!")
+
+_____  x x x _________
+T1 E1 
+ ask_name.ex 
+ 
+ defmodule AskName do
+
+  def nome() do
+
+     valorDigitado = IO.gets("Qual o seu nome?\n")
+
+     case String.trim(valorDigitado) do
+
+         "Nome da pessoa" ->
+		 IO.puts("Ola #{String.trim(valorDigitado)}, seja bem-vindo(a)!!")
+
+         _ ->
+             "Oi, #{String.trim(valorDigitado)}!"
+
+     end
+
+ end
